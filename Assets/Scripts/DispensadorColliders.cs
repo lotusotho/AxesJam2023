1f19ccc7ac7fceaaf72aeccf04e1cb81613aaaa2
Alejandro Ramos <lotusotho11@gmail.com>
2023-05-28 16:28:48 +0200

    Drinks and spawners

--- Assets/Scripts/DispensadorColliders.cs
+++ Assets/Scripts/DispensadorColliders.cs
@@ -12,6 +12,9 @@ public class DispensadorColliders : MonoBehaviour
     [SerializeField]
     private int _alimentoIndex;
 
+    //[SerializeField]
+    //private Transform[] _bebidaspawner;
+
     private void Start()
     {
         _dispensadorSpawner = GameObject.Find("DispensadorSpawner").transform;
@@ -19,9 +22,24 @@ public class DispensadorColliders : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag == "Player" && Input.GetButtonDown("Fire1"))
+        if (other.gameObject.tag == "Player" && Input.GetButtonDown("Fire1") && _alimentoIndex != 8 && _alimentoIndex != 9 && _alimentoIndex != 10)
         {
             Instantiate(Alimentos[_alimentoIndex], new Vector3(_dispensadorSpawner.transform.position.x + _alimentoIndex / 2.5f, _dispensadorSpawner.transform.position.y + .5f, _dispensadorSpawner.transform.position.z), _dispensadorSpawner.transform.localRotation);
+        }
+
+        if (other.gameObject.tag == "Player" && Input.GetButtonDown("Fire1") && _alimentoIndex == 8)
+        {
+            Instantiate(Alimentos[_alimentoIndex], transform);
+        }
+
+        if (other.gameObject.tag == "Player" && Input.GetButtonDown("Fire1") && _alimentoIndex == 9)
+        {
+            Instantiate(Alimentos[_alimentoIndex], transform);
+        }
+
+        if (other.gameObject.tag == "Player" && Input.GetButtonDown("Fire1") && _alimentoIndex == 10)
+        {
+            Instantiate(Alimentos[_alimentoIndex], transform);
         }
     }
 }
